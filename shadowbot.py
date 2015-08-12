bcfd47bb20d53f7bac5e68adaa46972ceb4237c8
Ramiro Bou <bouramiro@gmail.com>
2015-08-11 22:37:47 -0300

    Handling the bank, weight, etc

--- shadowbot.py
+++ shadowbot.py
@@ -20,25 +20,35 @@ hira.configure(server = config['server'], port=config['port'],
 hira.send("PASS " + config['password'])
 
 def on_hWelcome(cli, event):
-    cli.privmsg("Lamb3", "#hp")
+    cli.privmsg("Lamb3", "#we")
     
 def gotoHotel(cli, event):
     cli.privmsg("Lamb3", "#goto hotel")
 
+def gotoBank(cli, event):
+    cli.privmsg("Lamb3", "#goto bank")
+
 def gotToTheHotel(cli, event):
     cli.privmsg("Lamb3", "#sleep")
 
+def gotToTheBank(cli, event):
+    for _ in range(30):
+        cli.privmsg("Lamb3", "#push 3")
+
 HP_REGEX = re.compile("\002\d+\002-(.+?)\((.+?)\/(.+?)\)")
+WE_REGEX = re.compile(".+kg: \002\d+\002-(.+?)\((.+?)kg\/(.+?)kg\)")
 
 def on_privnotice(cli, event):
     if event.target != "Lamb3":
         return
     
     if event.arguments[0].endswith("but it seems you know every single corner of it."):
-        cli.privmsg("Lamb3", "#hp")
+        cli.privmsg("Lamb3", "#we")
     elif event.arguments[0].startswith("You enter the"):
         if "Hotel" in event.arguments[0]:
             gotToTheHotel(cli, event)
+        elif "Bank" in event.arguments[0]:
+            gotToTheBank(cli, event)
     elif event.arguments[0].startswith("You are ready to go."):
         cli.privmsg("Lamb3", "#exp")
     
@@ -58,9 +68,20 @@ def on_privmsg(cli, event):
         
         # Si todos están enteros, exploramos.
         cli.privmsg("Lamb3", "#explore")
+    if event.arguments[0].startswith("Your party carries"):
+        tx = HP_REGEX.findall(event.arguments[0].replace('Your party carries',''))
+        for jug in tx:
+            totes = (float(jug[1])/float(jug[2]))*100
+            if totes < 100:
+                # Si alguien es muy gordo, vamos al banco
+                gotoBank(cli, event)
+                return
+            cli.privmsg("Lamb3", "#hp")
     elif event.arguments[0].startswith("You are already in"):
         if "Hotel" in event.arguments[0]:
             gotToTheHotel(cli, event)
+        elif "Bank" in event.arguments[0]:
+            gotToTheBank(cli,event)
 
 hira.addhandler("welcome", on_hWelcome)
 hira.addhandler("privmsg", on_privmsg)
